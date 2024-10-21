665acf19c29254131c89e7f3d2ca571701bb7598
BorisKalev <64435381+BorisKalev@users.noreply.github.com>
2024-10-21 19:26:45 -0400

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <section className="lg:max-w-6xl mx-auto flex flex-col z-0 items-center justify-center py-28 sm:pt-32 transition-all animate-in">
+      <h1>Footer</h1>
+    </section>
+  );
+}
