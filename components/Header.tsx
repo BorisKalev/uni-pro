39112f1bce0f7117dc6ce5c82fe5e433db58d496
BorisKalev <64435381+BorisKalev@users.noreply.github.com>
2024-10-21 20:40:22 -0400

    Hero section finished but need images

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,23 @@
 import React from "react";
-
+import Image from "next/image";
+import Logo from "@/app/images/logo-unipro.png";
 export default function Header() {
   return (
-    <section className="lg:max-w-6xl mx-auto flex flex-col z-0 items-center justify-center py-28 sm:pt-32 transition-all animate-in">
-      <h1>Header</h1>
+    <section className="lg:max-w-6xl mx-auto">
+      <div className="flex justify-between items-center">
+        <Image
+          src={Logo}
+          alt="Uni-Pro-Logo"
+          className="w-[200px] h-[130px] object-contain"
+        />
+        <div className="flex items-center gap-16">
+          <p>About</p>
+          <p>Gallery</p>
+          <p>Pricing</p>
+          <p>Blog</p>
+          <p>Contact</p>
+        </div>
+      </div>
     </section>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,8 +2,27 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <section className="lg:max-w-6xl mx-auto flex flex-col z-0 items-center justify-center py-28 sm:pt-32 transition-all animate-in">
-      <h1>Hero</h1>
+    <section className="lg:max-w-6xl mx-auto ">
+      <div className="mt-16">
+        <div className="bg-red-500 p-2 w-[300px] h-[50px] skew-x-12 flex justify-center items-center">
+          <div className="-skew-x-12">
+            <h2 className="tracking-widest">BIENVENUE À UNI-PRO</h2>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col mt-8">
+        <h1 className="text-6xl max-w-3xl">
+          Votre partenaire de <span className="text-red-600">confiance</span>{" "}
+          pour l'entretien et la réparation automobile.
+        </h1>
+        <p className="opacity-50 mt-5 max-w-2xl">
+          Nous offrons des services fiables et efficaces pour garantir que votre
+          véhicule soit toujours en parfait état. Laissez-nous prendre soin de
+          votre voiture afin que vous puissiez vous concentrer sur ce qui compte
+          le plus.
+        </p>
+      </div>
     </section>
   );
 }
