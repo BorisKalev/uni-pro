b460bce0d690f70be63b051f962f27c8ed6c3313
BorisKalev <64435381+BorisKalev@users.noreply.github.com>
2024-11-04 17:47:14 -0500

    changement a l'animation

---
+++ components/CommentCaMarche.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CommentCaMarche() {
+  return (
+    <section className="lg:max-w-6xl mx-auto flex flex-col z-0 items-center justify-center py-28 sm:pt-32 transition-all animate-in">
+      <h1>Comment ca marche</h1>
+    </section>
+  );
+}
--- components/QueProposonsNous.tsx
+++ components/QueProposonsNous.tsx
@@ -12,17 +12,21 @@ export default function QueProposonsNous() {
           <div className="bg-gradient-to-l from-red-600 to-[#333333] h-[2px] mt-3" />
         </div>
 
-        <div className="flex items-center justify-center mt-[8rem] ">
+        <div className="flex items-center justify-center mt-[8rem]">
           {services.map((service) => (
-            <div className="flex flex-col">
+            <div key={service.id} className="flex flex-col items-center">
               <h1 className="text-center font-semibold">{service.title}</h1>
-              <div className="mt-5 mb-5 w-[300px] h-auto rounded-lg hover:border-x-8 hover:border-t-8 hover:border-b-[55px] hover:border-red-600 transition-all duration-500">
+
+              <div className="relative mt-5 mb-5 w-[300px] h-auto rounded-lg overflow-hidden group hover:border-x-8 hover:border-t-8 hover:border-b-[55px] hover:border-red-600 transition-all duration-500 z-10">
                 <Image
                   src={service.img}
                   alt={service.title}
-                  key={service.id}
-                  className="object-contain grayscale hover:grayscale-0"
+                  className="object-contain grayscale group-hover:grayscale-0"
                 />
+
+                <div className="absolute inset-0 flex items-center justify-center top-[130%] text-white font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-50">
+                  <h1>Voir plus</h1>
+                </div>
               </div>
             </div>
           ))}
