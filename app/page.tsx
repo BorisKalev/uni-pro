f7b92edaa691ea81fb7f3b71c9e5d0f456cf624f
BorisKalev <64435381+BorisKalev@users.noreply.github.com>
2024-11-21 19:37:29 -0500

    added the information of the footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import Footer from "@/components/Footer";
+import { Footer } from "@/components/Footer";
 import Services from "@/components/Services";
 import Apropos from "@/components/APropos";
 import CommentCaMarche from "@/components/CommentCaMarche";
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,48 @@
-import React from "react";
+import Image from "next/image";
+import logo from "@/app/images/logo-unipro.png";
+import SocialX from "@/app/images/social-x.svg";
+import SocialInsta from "@/app/images/social-insta.svg";
+import SocialLinkedIn from "@/app/images/social-linkedin.svg";
+import SocialPin from "@/app/images/social-pin.svg";
+import SocialYoutube from "@/app/images/social-youtube.svg";
 
-export default function Footer() {
+export const Footer = () => {
   return (
-    <section className="lg:max-w-6xl mx-auto flex flex-col z-0 items-center justify-center py-28 sm:pt-32 transition-all animate-in">
-      <h1>Footer</h1>
-    </section>
+    <footer className="bg-black text-[#BCBCBC] text-sm py-10 text-center mt-10">
+      <div className="container">
+        <div className="inline-flex relative">
+          <Image src={logo} alt="Saas logo" height={40} className="relative" />
+        </div>
+        <nav className="flex md:flex-row md:justify-center flex-col gap-6 mt-6">
+          <a href="#">About</a>
+          <a href="#">Features</a>
+          <a href="#">Customers</a>
+          <a href="#">Pricing</a>
+          <a href="#">Help</a>
+          <a href="#">Carreers</a>
+        </nav>
+
+        <div className="flex justify-center gap-6 mt-6">
+          <Image src={SocialX} alt="Social X Icon" width={24} height={24} />
+          <Image src={SocialInsta} alt="Social X Icon" width={24} height={24} />
+          <Image
+            src={SocialLinkedIn}
+            alt="Social X Icon"
+            width={24}
+            height={24}
+          />
+          <Image src={SocialPin} alt="Social X Icon" width={24} height={24} />
+          <Image
+            src={SocialYoutube}
+            alt="Social X Icon"
+            width={24}
+            height={24}
+          />
+        </div>
+        <p className="mt-6">
+          &copy; 2024 Your Company, Inc. All rights reserved.
+        </p>
+      </div>
+    </footer>
   );
-}
+};
