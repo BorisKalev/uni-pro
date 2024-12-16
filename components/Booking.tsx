13f722abd69860b1769aeb5724a84dfc11277e78
BorisKalev <64435381+BorisKalev@users.noreply.github.com>
2024-12-16 17:20:21 -0500

    added the booking part

--- components/Booking.tsx
+++ components/Booking.tsx
@@ -2,19 +2,15 @@ import Image from "next/image";
 import deal from "@/app/images/close-up-sales-manager-black-suit-selling-car-customer.jpg";
 export const Booking = () => {
   return (
-    <div className="">
-      <div className="flex">
-        <Image
-          src={deal}
-          alt="shaking hands"
-          className="w-[500px]  object-contain"
-        ></Image>
-
+    <div className="lg:max-w-6xl mx-auto pb-28">
+      <div className="flex relative">
         <div className="flex flex-col mx-auto">
-          <p className="text-red-600 tracking-wider">BOOK AN</p>
-          <h1 className="text-3xl">Appointement Form</h1>
+          <div className=" flex items-center flex-col">
+            <p className="text-red-600 tracking-wider">BOOK AN</p>
+            <h1 className="text-3xl">Appointement Form</h1>
+          </div>
 
-          <p className="mt-10">Contact info</p>
+          <p className="mt-10 text-red-600">Contact info</p>
           <div className="grid grid-cols-2 mt-5 gap-x-5 gap-y-2">
             <input type="text" placeholder="Your Name" />
             <input type="text" placeholder="Phone Number" />
@@ -22,14 +18,56 @@ export const Booking = () => {
             <input type="text" placeholder="Time" />
           </div>
 
-          <p className="mt-10">Car info</p>
+          <p className="mt-10 text-red-600">Car info</p>
           <div className="grid grid-cols-2 mt-5 gap-x-5 gap-y-2">
             <input type="text" placeholder="Maker" />
             <input type="text" placeholder="Model" />
           </div>
 
-          <p className="mt-10">Select Services Needed</p>
-          <div className="grid grid-cols-3"></div>
+          <p className="mt-10 text-red-600">Select Services Needed</p>
+          <div className="grid grid-cols-2 lg:grid-cols-3 mt-5 gap-y-5 gap-x-4">
+            <label className="flex items-center gap-2">
+              <input type="checkbox" value="Air Conditioner" />
+              Air Conditioner
+            </label>
+            <label className="flex items-center gap-2">
+              <input type="checkbox" value="Brake Repair" />
+              Brake Repair
+            </label>
+            <label className="flex items-center gap-2">
+              <input type="checkbox" value="Heating & Cooling" />
+              Heating & Cooling
+            </label>
+            <label className="flex items-center gap-2">
+              <input type="checkbox" value="Engine Diagnostics" />
+              Engine Diagnostics
+            </label>
+            <label className="flex items-center gap-2">
+              <input type="checkbox" value="Oil, Lube & Filters" />
+              Oil, Lube & Filters
+            </label>
+            <label className="flex items-center gap-2">
+              <input type="checkbox" value="Steering & Suspension" />
+              Steering & Suspension
+            </label>
+            <label className="flex items-center gap-2">
+              <input type="checkbox" value="Transmission Repair" />
+              Transmission Repair
+            </label>
+            <label className="flex items-center gap-2">
+              <input type="checkbox" value="Wheel Alignment" />
+              Wheel Alignment
+            </label>
+            <label className="flex items-center gap-2">
+              <input type="checkbox" value="Other" />
+              Other:
+              <input
+                type="text"
+                placeholder="Specify other services"
+                className="p-2 border border-gray-300 rounded w-full"
+              />
+            </label>
+          </div>
         </div>
       </div>
     </div>
